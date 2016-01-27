ec86a1d2ce69d69d6f4ee3d4f4d9280718999450
git <smendoza.barrera@gmail.com>
2016-01-27 17:25:39 -0600

    Beta testing v0.0.68

--- js/custom.js
+++ js/custom.js
@@ -2,9 +2,9 @@ function URLParser() {
     var x = decodeURIComponent(document.URL);
     var xsplit = x.split("");
 
-    if (xsplit[39] == "?" ) {
+    if (xsplit[32] == "?" ) {
         var newPathname = "";
-        for (i = 44; i < xsplit.length; i++) {
+        for (i = 37; i < xsplit.length; i++) {
             newPathname += xsplit[i];
         }
 
@@ -17,6 +17,3 @@ function URLParser() {
     var nameTextBox = document.getElementById("name");
     nameTextBox.value = nameTextBox.value + "Our best customer";
 }
-
-// http://testing02.bizland.biz/cform.html?cur=
-// http://testing02.bizland.biz/cform.html?cur=http%3A%2F%2Fwww.amazon.com%2FSeagate-Expansion-Desktop-External-STEB5000100%2Fdp%2FB00TKFEEBW%2Fref%3Dsr_1_3%3Fs%3Dpc%26ie%3DUTF8%26qid%3D1453912039%26sr%3D1-3%26keywords%3Dhard%2Bdrive
